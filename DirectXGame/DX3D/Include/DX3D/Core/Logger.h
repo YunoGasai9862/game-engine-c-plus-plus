5b4e0ee96e62eee954255ab4d2bd5d77564b75ec
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2026-05-24 19:22:04 +0300

    WIP (Logger)

---
+++ DirectXGame/DX3D/Include/DX3D/Core/Logger.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace dx3d{
+	class Logger final
+	{
+	public:
+		enum class LogLevel
+		{
+			Info = 0,
+			Warning = 1,
+			Error = 2
+		};
+
+		//to avoid implicit conversions (Logger logger = LogLevel::Error) -> Creating Logger with error log level
+		explicit Logger(LogLevel logLevel = LogLevel::Error);
+		void log(LogLevel level, const char* message) const;
+
+	private:
+		LogLevel m_logLevel = LogLevel::Error;
+	};
+}
---
+++ DirectXGame/DX3D/Source/DX3D/Core/Logger.cpp
@@ -0,0 +1,7 @@
+#include <DX3D/Core/Logger.h>
+
+dx3d::Logger::Logger(LogLevel logLevel): m_logLevel(logLevel)
+{}
+
+void dx3d::Logger::log(LogLevel level, const char* message) const
+{}
